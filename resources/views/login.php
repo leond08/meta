ecca704ac54fd75dca22f1035b7f569b39ff0569
leond08 <fryu771@gmail.com>
2017-01-19 14:18:48 +0800

    Third commit

--- resources/views/login.php
+++ resources/views/login.php
@@ -8,13 +8,13 @@
     <title>Meta | Lock</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
     <!--external css-->
-    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
         
     <!-- Custom styles for this template -->
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link href="assets/css/style-responsive.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet">
 	
    
   </head>
